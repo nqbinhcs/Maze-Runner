c3df0a735de7f7d3d8a6ce9966b91f7fffe5a486
php122002 <20120351@student.hcmus.edu.vn>
2021-12-15 08:09:54 +0700

    Change maze's parameters

--- MazeRunnerGame/MazeRunnerGame/DefinedVariables.hpp
+++ MazeRunnerGame/MazeRunnerGame/DefinedVariables.hpp
@@ -4,22 +4,23 @@
 //@DESCR: Parameters of screen
 
 //change
-#define SCREEN_HEIGHT 700 // change from 700 -> 960
+#define SCREEN_HEIGHT 850 // change from 700 -> 960
 #define SCREEN_WIDTH 1050 // change from 1050 -> 1280
 #define SIZE_ROW 13
 #define SIZE_COL 21
 #define FRAME 60
 #define NAME_GAME "MAZE RUNNER GAME"
 #define SIZE_CELL 50
+#define SPACE 10
 
 //=========================================
 // SCREEN_MAZE_WIDTH = MAZE_X * 32
 // SCREEN_MAZE_HEIGHT = MAZE_Y * 32
-#define SCREEN_MAZE_HEIGHT 640
-#define SCREEN_MAZE_WIDTH 640
+#define SCREEN_MAZE_HEIGHT 720
+#define SCREEN_MAZE_WIDTH 720
 
-#define OFFSET_MAZE_Y 50
-#define OFFSET_MAZE_X 50
+#define OFFSET_MAZE_Y (SCREEN_HEIGHT - SCREEN_MAZE_HEIGHT - SPACE)
+#define OFFSET_MAZE_X SPACE
 
 #define MAZE_Y 20
 #define MAZE_X 20
