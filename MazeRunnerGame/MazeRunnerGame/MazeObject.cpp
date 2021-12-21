2f02772e5d19984bbdd84ae52b852f4a90939e45
VuVanThai1410 <75356837+VuVanThai1410@users.noreply.github.com>
2021-12-21 22:17:39 +0700

    update MazeObject.cpp

--- MazeRunnerGame/MazeRunnerGame/MazeObject.cpp
+++ MazeRunnerGame/MazeRunnerGame/MazeObject.cpp
@@ -1,60 +1,11 @@
 #include "MazeObject.hpp"
 
-void MazeObject::initVariables()
-{
-
-}
-
-void MazeObject::initShape()
-{
-	this->shape.setFillColor(sf::Color::White);
-	this->shape.setSize(sf::Vector2f(OBJECT_WIDTH, OBJECT_HEIGHT));
-}
-
-
-MazeObject::MazeObject(float x, float y)
-{
-	this->shape.setPosition(x, y);
-
-	this->initVariables();
-	this->initShape();
-}
-
-float MazeObject::getPositionX() const
-{
-	return this->shape.getPosition().x;
-}
-
-float MazeObject::getPositionY() const
-{
-	return this->shape.getPosition().y;
-}
-
-void MazeObject::setPosition(float x, float y)
-{
-	this->shape.setPosition(x, y);
-}
-
-void MazeObject::setColor(sf::Color color)
-{
-	this->shape.setFillColor(color);
-}
-
-void MazeObject::setSize(float width, float height)
-{
-	this->shape.setSize(sf::Vector2f(width, height));
-}
-
-void MazeObject::setOutLine(float think, sf::Color color) {
-	this->shape.setOutlineThickness(think);
-	this->shape.setOutlineColor(sf::Color(color));
-}
-
 void MazeObject::update(const sf::RenderTarget* target)
 {
 }
 
-void MazeObject::render(sf::RenderTarget* target)
-{
-	target->draw(this->shape);
+void MazeObject::render(sf::RenderWindow& window) {
+	sf::Sprite curRoomSprite(curTexure);
+	curRoomSprite.setPosition(xOffset + (int)((curMazePosition.getX()) * width), yOffset + (int)((curMazePosition.getY()) * height));
+	window.draw(curRoomSprite);
 }
