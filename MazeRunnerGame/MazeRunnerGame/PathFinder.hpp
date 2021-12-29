8f2ab66fb4237c899504c7ba045325ab2f15088d
php122002 <80665411+php122002@users.noreply.github.com>
2021-12-29 23:27:26 +0700

    Refactor code

--- MazeRunnerGame/MazeRunnerGame/PathFinder.hpp
+++ MazeRunnerGame/MazeRunnerGame/PathFinder.hpp
@@ -9,3 +9,38 @@ public:
 	virtual vector<shared_ptr<Room>> findPath(shared_ptr<Room> start, shared_ptr<Room> end, bool option = 0) = 0;
 };
 
+#pragma once
+#include "PathFinder.hpp"
+
+enum AlgorithmIndex
+{
+	iBFS = 0,
+	iDFS = 1,
+	iAStar = 2
+};
+
+class BFS : public PathFinder
+{
+public:
+	vector<shared_ptr<Room>> findPath(shared_ptr<Room> start, shared_ptr<Room> end, bool option = 0);
+};
+
+class DFS : public PathFinder
+{
+public:
+	vector<shared_ptr<Room>> findPath(shared_ptr<Room> start, shared_ptr<Room> end, bool option = 0);
+};
+
+//class Dijkstra : public PathFinder
+//{
+//public:
+//	static vector<shared_ptr<Room>> findPath(shared_ptr<Room> start, shared_ptr<Room> end, bool option = 0);
+//};
+
+class AStar : public PathFinder
+{
+public:
+	int calcH(shared_ptr<Room> room, shared_ptr<Room> end);
+	vector<shared_ptr<Room>> findPath(shared_ptr<Room> start, shared_ptr<Room> end, bool option = 0);
+};
+
